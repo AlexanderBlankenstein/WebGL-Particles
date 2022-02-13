d75404543152f3b33b77497853b20d6e0d6a5c6a
Alexander Blankenstein <a.blankenstein@live.com>
2022-02-12 23:18:05 -0500

    started implementation of vertices array.

--- loaddata.js
+++ loaddata.js
@@ -4,6 +4,8 @@ var frameCount = 0;  // number of frames drawn
 var drawState = 1;   // flag the controls which object drawn
 var ht = 0.1;        // initial height of object, changed by keyboard input
 
+var step = 0;
+
 //var reinitalizeSystem = ?;
 var numberParticles = 1;
 var directionOffset = 0.05;
@@ -12,7 +14,7 @@ var ageVariation = 0;
 var repeating = true;
 var path = true;
 
-
+//Particle object. 
 function Particle(x, y, z, xOff, zOff, curAge, maxAge) {
 	this.position = (x,y,z);
 	this.offsets = (xOff,0.1,zOff);
@@ -20,6 +22,9 @@ function Particle(x, y, z, xOff, zOff, curAge, maxAge) {
 	this.maxAge = maxAge;
 }
 
+//create a list of particles based off random settings within global ranges. 
+//inputs number of particles requested, 
+//returns list of particles (array).
 function makeParticles(num) {
 	var particles = new Array(num);
 	for (let i=0; i<num; i++) {
@@ -36,6 +41,12 @@ function makeParticles(num) {
 	return particles;
 }
 
+function ageParticle(Particle) {
+	Particle.currentAge += 1;
+	particle.position = (Particle.position[0] + Particle.offsets[0],Particle.position[1] + Particle.offsets[1],Particle.position[2] + Particle.offsets[2]);
+}
+
+//reset particle with new randomized values.
 function resetParticle(Particle) {
 	let x = Math.random() * (-2 - 2) + 2;
 	let y = -1.0;
@@ -59,53 +70,81 @@ function getVertexCount() {
 	// vertex positions
 function loadvertices() {
 
+	if (step == 0) {
+		var particalList = makeParticles(numberParticles);
+	}
+
 	// every 25 frames causes the object being drawn to change to other
 	// object
 	// this can be removed for the assignment
 	if (frameCount % 25 == 0) {
-		drawState *= -1;
+		//drawState *= -1;
+		step += 1;
 	}
+	
+	if (step != 1) {
+		for (let i=0; i<particalList.length; i++) {
+			ageParticle(particalList[i]);
+		}
+	}
+	
+	var toReturn = new Array();
+	for (let i=0; i<particalList.length; i++) {
+		var x = particalList[i].position[0];
+		var y = particalList[i].position[1];
+		var z = particalList[i].position[2];
+		toReturn.push(	
+			x, (y-0.1), z,
+			x, y,  z,
+			x, (y-0.1), (z-0.1),
+		
+			x, (y-0.1), (z-0.1),
+			x, y,  z,
+			x, y, (z-0.1),
+		);
+	}
+	return toReturn;
 
 	// use drawState to alternate between the objects (high and low objects)
 	// you will need to add more complex state control for the assignment
 	// all of the other loading function below do the same thing
-	if (drawState == 1) {
-		return [
-			-2.0, -ht, -2.0,
-			-2.0, 0.0,  -2.0,
-			-2.0, -ht, -2.1,
+	//if (drawState == 1) {
+	//	return [
+	//		-2.0, -ht, -2.0,
+	//		-2.0, 0.0,  -2.0,
+	//		-2.0, -ht, -2.1,
 
-			-2.0, -ht, -2.1,
-			-2.0, 0.0,  -2.0,
-			-2.0, 0.0, -2.1,
+	//		-2.0, -ht, -2.1,
+	//		-2.0, 0.0,  -2.0,
+	//		-2.0, 0.0, -2.1,
 
-			2.0, -ht,  2.0,
-			2.0, 0.0,   2.0,
-			2.0, -ht, 2.1,
+	//		2.0, -ht,  2.0,
+	//		2.0, 0.0,   2.0,
+	//		2.0, -ht, 2.1,
 
-			2.0, -ht, 2.1,
-			2.0, 0.0,   2.0,
-			2.0, 0.0,  2.1,
-		];
-	} else {
-		return [
-			-2.0, 0.0, -2.0,
-			-2.0, ht,  -2.0,
-			-2.0, 0.0, -2.1,
+	//		2.0, -ht, 2.1,
+	//		2.0, 0.0,   2.0,
+	//		2.0, 0.0,  2.1,
+	//	];
+	//} else {
+	//	return [
+	//		-2.0, 0.0, -2.0,
+	//		-2.0, ht,  -2.0,
+	//		-2.0, 0.0, -2.1,
 
-			-2.0, 0.0, -2.1,
-			-2.0, ht,  -2.0,
-			-2.0, ht, -2.1,
+	//		-2.0, 0.0, -2.1,
+	//		-2.0, ht,  -2.0,
+	//		-2.0, ht, -2.1,
 
-			2.0, 0.0,  2.0,
-			2.0, ht,   2.0,
-			2.0, 0.0,  2.1,
+	//		2.0, 0.0,  2.0,
+	//		2.0, ht,   2.0,
+	//		2.0, 0.0,  2.1,
 
-			2.0, 0.0,  2.1,
-			2.0, ht,   2.0,
-			2.0, ht,  2.1,
-		];
-	}
+	//		2.0, 0.0,  2.1,
+	//		2.0, ht,   2.0,
+	//		2.0, ht,  2.1,
+	//	];
+	//}
 }
 
 
